3f17de3f8575a4bac0a8163683293b532ae647db
aluno <aluno@172.16.3.55>
2022-12-06 16:35:54 -0300

    commit fatura

---
+++ ProjetoJava/src/Fatura.java
@@ -0,0 +1,37 @@
+
+public class Fatura {
+
+	private int faturald;
+	private int mesAno;
+	private int consumo;
+	private boolean pago;
+	private UC unidadeConsumidora; 
+	
+	
+	public int getFaturald() {
+		return faturald;
+	}
+	public void setFaturald(int faturald) {
+		this.faturald = faturald;
+	}
+	public int getMesAno() {
+		return mesAno;
+	}
+	public void setMesAno(int mesAno) {
+		this.mesAno = mesAno;
+	}
+	public int getConsumo() {
+		return consumo;
+	}
+	public void setConsumo(int consumo) {
+		this.consumo = consumo;
+	}
+	public boolean isPago() {
+		return pago;
+	}
+	public void setPago(boolean pago) {
+		this.pago = pago;
+	}
+	
+	
+}
