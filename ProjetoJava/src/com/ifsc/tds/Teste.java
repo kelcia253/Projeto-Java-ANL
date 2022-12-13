61642419ee60f94df061fa3fff9af9cb268435ad
noemi1310 <aluno@172.16.3.24>
2022-12-13 16:52:40 -0300

    menu

--- ProjetoJava/src/com/ifsc/tds/Teste.java
+++ ProjetoJava/src/com/ifsc/tds/Teste.java
@@ -1,11 +1,32 @@
 package com.ifsc.tds;
 
+import java.util.Scanner;
+
 public class Teste {
 
 	public static void main(String[] args) {
 		
+	System.out.println("-----------------------\n"
+			         + "1) CADASTRAR CLIENTE\n"
+			         + "2)CADASTRAR UNIDADES CONSUMIDORAS\n"
+			         + "3)FAZER LEITURA\n"
+			         + "4)FATURAR CONSUMO\n"
+			         + "5)EXIBIR RELATORIOS\n"
+			         );
+					
+	Scanner teclado = new Scanner(System.in);
+	
+	int opcao = teclado.nextInt();
+	if(opcao == 1) {
 		
-
+	System.out.println("Digite o nome do cliente: \n");
+	setNome
+	
+	
+	
+	}
+	
+	
 	}
 
 }
--- ProjetoJava/src/com/ifsc/tds/UC.java
+++ ProjetoJava/src/com/ifsc/tds/UC.java
@@ -9,6 +9,36 @@ public class UC {
 	private int leituraAnterior;
 	private int leituraAtual;
 	private Endereco Endereco;
+	public int getUCid() {
+		return UCid;
+	}
+	public void setUCid(int uCid) {
+		UCid = uCid;
+	}
+	public Cliente getCliente() {
+		return Cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		Cliente = cliente;
+	}
+	public int getLeituraAnterior() {
+		return leituraAnterior;
+	}
+	public void setLeituraAnterior(int leituraAnterior) {
+		this.leituraAnterior = leituraAnterior;
+	}
+	public int getLeituraAtual() {
+		return leituraAtual;
+	}
+	public void setLeituraAtual(int leituraAtual) {
+		this.leituraAtual = leituraAtual;
+	}
+	public Endereco getEndereco() {
+		return Endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		Endereco = endereco;
+	}
 	
 	
 	
